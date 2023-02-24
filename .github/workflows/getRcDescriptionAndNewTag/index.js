7dcc7a718c58adc8689f0ed3d974920a8a0ad912
matejgerek <maftvej@gmail.com>
2023-02-24 22:01:20 +0100

    initial commit

---
+++ .github/workflows/getRcDescriptionAndNewTag/index.js
@@ -0,0 +1,28 @@
+import {exec} from "child_process";
+import {promisify} from "util";
+import semanticRelease from "semantic-release";
+import * as core from "@actions/core";
+
+const execAsync = promisify(exec);
+
+const getRcDescriptionAndNewTagWithSemanticRelease = async () => {
+    try {
+        const result = await semanticRelease({
+            plugins: [
+                ["@semantic-release/release-notes-generator", {
+                    "preset": "conventionalcommits",
+                }],
+                "@semantic-release/git"
+                ],
+        },
+        );
+
+        console.log(result);
+        core.setOutput('NEW_VERSION', result.nextRelease.version);
+        core.setOutput('NEW_CHANGES', result.nextRelease.notes);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+getRcDescriptionAndNewTagWithSemanticRelease();
