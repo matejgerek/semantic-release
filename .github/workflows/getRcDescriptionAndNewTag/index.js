235726a03d91720ef815be7a2e5934cd0bd42765
matejgerek <maftvej@gmail.com>
2023-03-06 08:03:20 +0100

    feat: test feature

--- .github/workflows/getRcDescriptionAndNewTag/index.js
+++ .github/workflows/getRcDescriptionAndNewTag/index.js
@@ -25,7 +25,6 @@ const getRcDescriptionAndNewTagWithSemanticRelease = async () => {
 
         // next release version of prerelease branch consists of {version}-{postfix}
         const newVersion = result.nextRelease.version.split('-')[0]
-
         core.setOutput('NEW_VERSION', newVersion);
         core.setOutput('NEW_CHANGES', result.nextRelease.notes);
     } catch (error) {
